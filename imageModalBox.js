d7e41dc2e11e5eb4187e7ce574919ff800506c35
naokis <naokis@ranadesign.com>
2011-08-05 16:20:22 +0900

    Change data structure, from json data to html inline data. And remake class structure.

--- imageModalBox.js
+++ imageModalBox.js
@@ -6,30 +6,14 @@
  * @copyright  Naoki Sekiguchi (http://likealunatic.jp)
  * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
  * @version    Release: 1.0
- * @update     2011-07-19 00:20:15
+ * @update     2011-08-01 18:18:41
  */
 
 (function($) {
 
-$.fn.imageModalBox = function (options) {
-	this.each(function (i, element) {
-		var $element = $(element);
-		options.element = $element;
-		options.dataSrc = $element.data('images');
-		$element.data('imageModalBox', new ImageModalBox(options));
-	});
-};
-
-function ImageModalBox() {
-	// extend default options
-	$.extend(this, this.defaultOptions, arguments[0]);
-	// initialize
-	this._create();
-}
-
-ImageModalBox.prototype = {
-	defaultOptions: {
-		dataSrc: '',
+$.fn.imageModalBox = function (argOptions) {
+	// default options
+	var options = {
 		element: {},
 		containerClassName: 'modal_container',
 		overlayClassName: 'modal_overlay',
@@ -37,134 +21,84 @@ ImageModalBox.prototype = {
 		headerClassName: 'modal_header',
 		contentClassName: 'modal_content',
 		prevNavClassName: 'modal_prev',
-		nextNavClassName: 'modal_next'
-	},
+		nextNavClassName: 'modal_next',
+		closeNavClassName: 'modal_close',
+		loadingImgSrc: 'loading.gif',
+		loadingImgClassName: 'modal_loading',
+		maxHeight: 600
+	};
+	// extend default options
+	$.extend(options, argOptions);
+	options.element = this;
+	// main object
+	var imb = new ImageModalBox(options);
+	// object to process each group
+	imb.group = new ImageModalBoxGroup(options);
+};
 
+function ImageModalBox() {
+	// extend this object with options
+	$.extend(this, arguments[0]);
+	// object to save ImageModalBoxGroup object
+	this.group = {};
+	// initialize
+	this._create();
+}
+
+ImageModalBox.prototype = {
 	_create: function () {
-		this.imageIndex = 0;
-		this.container = $('div.' + this.containerClassName);
-		this.overlay = $('div.' + this.overlayClassName);
-		$.ajax(this.dataSrc, {
-			cache: false,
-			context: this,
-			success: function (dataArray) {
-				this.dataArray = dataArray;
-				if (!this.container[0] || !this.overlay[0]) {
-					this._init(dataArray);
-				}
-				this._buildHTML(dataArray);
-			}
-		});
-
 		this.isIE7 = ($.browser.msie && $.browser.version < 8);
-//        var url = this.element.attr('href');
-
-		this.element.click($.proxy(function (e) {
-			this._openModal(0);
-			$(document).bind('keydown', $.proxy(this._keyEventHandler, this));
-			e.preventDefault();
-		}, this));
-		this.overlay.click($.proxy(function (e) {
-			this._closeModal();
-			$(document).unbind('keydown', $.proxy(this._keyEventHandler, this));
-			e.preventDefault();
-		},this));
-	},
-
-	_init: function (dataArray) {
 		this.container = $('<div/>', {
 				'class': this.containerClassName
-			}).hide()
-			.append(this.header)
-			.append(this.content)
-			.appendTo(document.body);
+			}).hide().appendTo(document.body);
 		this.overlay = $('<div/>', {
 				'class': this.overlayClassName
 			}).hide().appendTo(document.body);
-		this.prevNav = $('<div/>', {
-				'class': this.prevNavClassName
-			}).click($.proxy(function (e) {
-				this._switchImage('prev');
-			},this)).hide().appendTo(document.body);
-		this.nextNav = $('<div/>', {
-				'class': this.nextNavClassName
-			}).click($.proxy(function (e) {
-				this._switchImage('next');
-			},this)).hide().appendTo(document.body);
-	},
+		this.closeNav = $('<div/>', {
+				'class': this.closeNavClassName
+			}).hide().appendTo(document.body);
+		this.loadingImg = $('<img/>', {
+			'src': this.loadingImgSrc,
+			'class': this.loadingImgClassName
+		}).hide().appendTo(document.body);
 
-	_buildHTML: function (dataArray) {
-		var section, header, content;
-		this.sections = [];
-
-		$.each(dataArray, $.proxy(function (i, data) {
-			section = $('<div/>', {
-				'class': this.sectionClassName
-			});
-			this.sections[i] = section;
-			this._buildImageSection(section, data);
+		this.element.each($.proxy(function (i, element) {
+			$(element).click($.proxy(function (e) {
+				this.group.imageIndex = i;
+				this._openModal();
+				$(document).bind('keydown', $.proxy(this._keyEventHandler, this));
+				e.preventDefault();
+			}, this));
 		}, this));
-		
+		this.overlay.click($.proxy(function (e) {
+			this._closeModal();
+			e.preventDefault();
+		},this));
+		this.closeNav.click($.proxy(function (e) {
+			this._closeModal();
+		}, this));
 	},
-	
-	_buildImageSection: function (section, data) {
-		var winWidth = $(window).width(),
-			winHeight = $(window).height(),
-			src = data.src,
-			minWidth = data.minWidth || winWidth,
-			minHeight = data.minHeight || winHeight,
-			title = data.title || '',
-			description = data.description || '',
-			header = $('<div/>', {
-					'class': this.headerClassName
-			}),
-			content = $('<div/>', {
-					'class': this.contentClassName
-			});
-		if (!src) { return false; }
 
-		// build header
-		var titleWrap = $('<div/>', {
-			text: title
-		}),
-		descriptionWrap = $('<div/>', {
-			text: description
-		});
-		header.append(titleWrap).append(descriptionWrap);
-
-		// done
-		$(section).append(header).append(content);
-	},
-	
-	_loadImage: function (data, wrapper) {
-		// build content
-		var img = $('<img/>', {
-			'src': src
-		}).css({
-			'visibility': 'hidden'
-		}).appendTo(content);
-		img.load(function (e) {
-console.log($(this).width());
-		});
-	},
-	
 	// close modal with Esc key
 	_keyEventHandler: function (e) {
 		var key = e.keyCode || e.charCode;
 		if (key == 27) {
 			this._closeModal();
-			$(document).unbind('keydown');
 		}
 	},
 
 	_openModal: function () {
 		var winWidth = $(window).width(),
-			winHeight = $(window).height(),
-			$container = this.container;
+			winHeight = $(window).height();
+		/**
+		 * delegate process to ImageModalBoxGroup object
+		 */
+		this.group.container = this.container;
+		this.group.loadingImg = this.loadingImg;
+		this.group.switchImage();
 
-		$container.append(this.sections[0]);
-		$container.show();
-		this._updateNav();
+		this.container.show();
+		this.closeNav.show();
 		if (this.isIE7) {
 			this.overlay.width(winWidth).height(winHeight).show();
 		} else {
@@ -172,35 +106,77 @@ console.log($(this).width());
 		}
 	},
 
-	_closeModal: function () {//{{{
+	_closeModal: function () {
 		this.container.empty().hide();
-		this.prevNav.hide();
-		this.nextNav.hide();
+		this.closeNav.hide();
+		this.group.prevNav.hide();
+		this.group.nextNav.hide();
 		if (this.isIE7) {
 			this.overlay.hide();
 		} else {
 			this.overlay.fadeOut();
 		}
-	},//}}}
+		$(document).unbind('keydown', $.proxy(this._keyEventHandler, this));
+	}
+};
 
-	_switchImage: function (direction) {//{{{
-		if (direction == 'prev') {
-			// prev
-			this.imageIndex --;
-			this.container.empty()
-				.append(this.sections[this.imageIndex]);
-			this._updateNav();
-		} else {
-			// next
-			this.imageIndex ++;
-			this.container.empty()
-				.append(this.sections[this.imageIndex]);
-			this._updateNav();
-		}
-	},//}}}
 
+function ImageModalBoxGroup() {
+	// extend this object with options
+	$.extend(this, arguments[0]);
+	// initialize
+	this._create();
+}
+ImageModalBoxGroup.prototype = {
+	_create: function () {
+		this.imageIndex = 0;
+		this.sections = [];
+		this.element.each($.proxy(function (i, element) {
+			this.sections[i] = this._buildImageSection($(element));
+		}, this));
+
+		this.prevNav = $('<div/>', {
+				'class': this.prevNavClassName
+			}).click($.proxy(function (e) {
+				this.imageIndex --;
+				this.switchImage();
+			},this)).hide().appendTo(document.body);
+		this.nextNav = $('<div/>', {
+				'class': this.nextNavClassName
+			}).click($.proxy(function (e) {
+				this.imageIndex ++;
+				this.switchImage();
+			},this)).hide().appendTo(document.body);
+	},
+
+	_buildImageSection: function (element) {//{{{
+		var title = element.data('title') || '',
+			description = element.data('description') || '',
+			section = $('<div/>', {
+				'class': this.sectionClassName
+			}),
+			header = $('<div/>', {
+				'class': this.headerClassName
+			}),
+			// build header
+			titleWrap = $('<div/>', {
+				'class': 'title',
+				'text': title
+			}),
+			descriptionWrap = $('<div/>', {
+				'class': 'description',
+				'text': description
+			});
+		this.content = $('<div/>', {
+			'class': this.contentClassName
+		});
+		header.append(titleWrap).append(descriptionWrap);
+		// return section jQuery object
+		return section.append(header).append(this.content);
+	},
+//}}}
 	_updateNav: function () {//{{{
-		var lastIndex = this.dataArray.length - 1;
+		var lastIndex = this.sections.length - 1;
 		if (this.imageIndex <= 0) {
 			this.prevNav.hide();
 		} else {
@@ -211,7 +187,81 @@ console.log($(this).width());
 		} else {
 			this.nextNav.show();
 		}
-	}//}}}
+	},
+//}}}
+	switchImage: function () {
+		var section = this.sections[this.imageIndex];
+		this.loadingImg.show();
+		/**
+		 * load Image.
+		 * delegate process to ImageModalBoxGroup.Item object
+		 */
+		this.item = new ImageModalBoxGroup.Item({
+				src: $(this.element[this.imageIndex]).attr('href') || '',
+				alt: $(this.element[this.imageIndex]).data('title') || '',
+				maxHeight: this.maxHeight,
+				loaded: $.proxy(function() {
+					this.loadingImg.hide();
+				}, this)
+			});
+		// rebuild HTML
+		section.find('div.' + this.contentClassName)
+			.empty().append(this.item.getImg());
+		this.container.empty().append(section);
+		this._updateNav();
+	}
+};
+
+/**
+ * each item constructor
+ */
+ImageModalBoxGroup.Item = function () {
+	// extend this object with options
+	$.extend(this, this.defaultOptions, arguments[0]);
+	// initialize
+	this._create();
+};
+ImageModalBoxGroup.Item.prototype = {
+	defaultOptions: {
+		src: '',
+		alt: '',
+		maxHeight: 0,
+		loaded: function() { }
+	},
+	_create: function () {
+		this.img = $('<img/>', {
+			'src': this.src,
+			'alt': this.alt
+		}).css({
+			'visibility': 'hidden'
+		});
+		this.img.load($.proxy(function (e) {
+			this._load();
+		}, this));
+	},
+	_load: function () {
+		var imgWidth = $(this.img).width(),
+			imgHeight = $(this.img).height();
+			hvRatio = imgWidth / imgHeight;
+		if (imgHeight > this.maxHeight) {
+			imgHeight =  this.maxHeight;
+		}
+		this.loaded();
+		this.img.css({
+			'visibility': 'visible',
+			'height': imgHeight
+		});
+	},
+	getImg: function () {
+		return this.img;
+	}
 };
 
 })(jQuery);
+
+
+/**
+ * ToDo:
+ * switchImage するときにフェードなどを入れる
+ */
+
